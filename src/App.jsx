9357d862cbb86e3ba3dbe27c49e7fb97cf28f97f
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2023-03-29 22:45:44 -0300

    AT3

--- src/App.jsx
+++ src/App.jsx
@@ -1,117 +1,55 @@
 import './App.css';
-import {Button, Card, Footer, Label, TextInput} from "flowbite-react";
-import {useEffect, useState} from "react";
+import {Button, Card, Footer, Label} from "flowbite-react";
+import {useState} from "react";
 
+function sortear(quantos) {
+  const numeros = [];
+  while (quantos > 0) {
+    const novo = Math.floor(Math.random() * 60 + 1);
+    if (numeros.includes(novo)) continue;
+    numeros.push(novo);
+    quantos -= 1;
+  }
+  numeros.sort((a, b) => (a > b ? +1 : -1));
+  return numeros;
+}
 export default function App() {
-  const [estado, setEstado] = useState({
-    consumo: '100',
-    pessoas: '4',
-    gorjetaPorcento: '10'
-  });
-  const [resultado, setResultado] = useState({
-    estado: 'aguardando',
-    totalFinal: '0',
-    gorjetaReais: '0',
-    porPessoa: '0',
-    erro: null
-  });
-
-  const handleChange = ({target: {id, value}}) => {
-    setEstado({...estado, [id]: value});
-  };
-
-  const {consumo, pessoas, gorjetaPorcento} = estado;
-
-  useEffect(() => {
-    if (consumo === '' || pessoas === '' || gorjetaPorcento === '') return setResultado({estado: 'aguardando'});
-
-    const consumoNum = parseFloat(consumo);
-    const pessoasNum = parseInt(pessoas);
-    const gorjetaPorcentoNum = parseInt(gorjetaPorcento);
-    if (isNaN(consumoNum) || isNaN(pessoasNum) || isNaN(gorjetaPorcentoNum)) return setResultado({
-      estado: 'erro',
-      erro: 'Valores inválidos'
-    });
-
-    const gorjetaReais = consumoNum * (gorjetaPorcento / 100);
-    const totalFinal = consumoNum + gorjetaReais;
-    const porPessoa = totalFinal / pessoas;
-
-    setResultado({
-      estado: 'pronto',
-      totalFinal,
-      gorjetaReais,
-      porPessoa,
-    });
-  }, [estado]);
+  const [quantos, setQuantos] = useState(6);
+  const [range, setRange] = useState(6);
+  const rangeConvertido = Math.floor(60 * range / 100);
+  const [numeros, setNumeros] = useState([]);
 
   return (
     <>
       <main className="flex flex-col items-center justify-center gap-4">
-        <div>
-          <div className="mb-2 block">
-            <Label
-              htmlFor="consumo"
-              value="Soma do consumo da mesa"
-            />
-          </div>
-          <TextInput
-            id="consumo"
-            type="number"
-            placeholder="100"
-            value={consumo}
-            required={true}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <div className="mb-2 block">
-            <Label
-              htmlFor="pessoas"
-              value="Dividir em quantas pessoas"
-            />
-          </div>
-          <TextInput
-            id="pessoas"
-            type="number"
-            placeholder="4"
-            value={pessoas}
-            required={true}
-            onChange={handleChange}
-          />
-        </div>
-
+        <h1 className="text-center text-4xl">Mega Sena</h1>
         <Label
-          htmlFor="gorjetaPorcento"
-          value={`Gorjeta (${gorjetaPorcento}%)`}
+          htmlFor="quantos"
+          value={`Números para sortear entre 0 e 60: ${rangeConvertido}`}
         />
-        <input id="gorjetaPorcento"
+        <input id="quantos"
                type="range"
-               value={gorjetaPorcento}
-               onChange={handleChange}
-               className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700 my-4"/>
+               value={range}
+               onChange={({target: {value}}) => {
+                 setRange(parseInt(value));
+               }}
+               className="max-w-xl h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700 my-4"
+        />
+        <Button
+          type="button"
+          onClick={() => {
+            setQuantos(rangeConvertido);
+            setNumeros(sortear(rangeConvertido));
+          }}>Sortear</Button>
 
-        {resultado.estado === 'pronto' && (
-          <Card>
+        {numeros.length > 0 && (
+          <Card className="max-w-2xl">
             <h2 className="text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               Resultado
             </h2>
             <p className="font-normal text-gray-700 dark:text-gray-400 text-right">
-              Consumo: R$ {parseFloat(consumo).toFixed(2).replace('.', ',')}
-              <br/>
-              Gorjeta: R$ {resultado.gorjetaReais.toFixed(2).replace('.', ',')}
-              <br/>
-              Total com gorjeta: R$ {resultado.totalFinal.toFixed(2).replace('.', ',')}
-              <br/>
-              Total por pessoa: R$ {resultado.porPessoa.toFixed(2).replace('.', ',')}
+              {numeros.join(', ')}
             </p>
-          </Card>
-        )}
-        {resultado.estado === 'erro' && (
-          <Card href="#">
-            <h2 className="text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {resultado.erro}
-            </h2>
           </Card>
         )}
       </main>
